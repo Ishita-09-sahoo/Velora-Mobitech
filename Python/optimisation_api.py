964ab19d8b977dcee2ee64a7724e2e4586872076
Ishita-09-sahoo <ishita.sahoo0901@gmail.com>
2026-03-14 18:57:52 +0530

    little fixes

--- Python/optimisation_api.py
+++ Python/optimisation_api.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI, UploadFile, File
 import shutil
 import os
+import uuid
 from main import run_optimisation
 from fastapi.middleware.cors import CORSMiddleware
 
@@ -14,7 +15,7 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-UPLOAD_FOLDER = "uploads"
+UPLOAD_FOLDER = "/tmp/uploads"
 
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 
@@ -25,6 +26,7 @@ def health():
 @app.post("/optimise")
 async def optimise(file: UploadFile = File(...)):
 
+    filename = f"{uuid.uuid4()}_{file.filename}"
     path = os.path.join(UPLOAD_FOLDER, file.filename)
 
     with open(path, "wb") as buffer:
